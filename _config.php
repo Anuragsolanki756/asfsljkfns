b0b57074235015bfdfa468584e5a4b62f267cf37
Yashraj Solanki <120442141+Anuragsolanki756@users.noreply.github.com>
2023-02-05 20:43:00 +0530

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "9xAnime"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png"; // Logo
+$contactEmail = "xyz@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/NinexAnime"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
